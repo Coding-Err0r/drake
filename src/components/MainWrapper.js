4c1a7bfe2c4e44afe236fd02e9b266b30b008d95
Rhine <rhine.cse@gmail.com>
2023-07-30 13:35:45 +0600

    ⚒️ Fix : Faq only one card will open

--- src/components/MainWrapper.js
+++ src/components/MainWrapper.js
@@ -2,7 +2,6 @@ import React from "react";
 import Lsb from "./Global-content/Lsb";
 import Herosec from "./Main-Components/Herosec";
 import About from "./Main-Components/About";
-import Resume from "./Main-Components/Resume";
 import Service from "./Main-Components/Service";
 import Skills from "./Main-Components/Skills";
 import Portfolio from "./Main-Components/Portfolio";
@@ -10,16 +9,21 @@ import Testimonials from "./Main-Components/Testimonials";
 import Clientlogos from "./Main-Components/Clientlogos";
 import Pricing from "./Main-Components/Pricing";
 import Contact from "./Main-Components/Contact";
+import SocialMedia from "./Main-Components/SocialMedia";
+import Education from "./Main-Components/Education";
+import Experience from "./Main-Components/Experience";
 
 export default function Main() {
   return (
     <main className="drake-main">
       <div id="smooth-wrapper">
         <div id="smooth-content">
-          <Lsb /> {/* this component will used by mobile devices */}
+          <Lsb />
+          <SocialMedia />
           <Herosec />
           <About />
-          <Resume />
+          <Education />
+          <Experience />
           <Service />
           <Skills />
           <Portfolio />
